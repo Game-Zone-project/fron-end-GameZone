515c6c6e2b5c73841cd764165e6a5680460adcc8
WalaaAlrefai <lulusedqi@gmail.com>
2023-04-17 23:36:33 +0000

    try to pull new work

--- src/components/AddToWishList/AddToWishList.js
+++ src/components/AddToWishList/AddToWishList.js
@@ -1,43 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import './AddToWishList.css';
-import WishListCard from '../wishListCard/WishListCard';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
+import { useEffect, useState } from "react";
+// import { useRef } from 'react';
+
+
 
 export default function AddToWishList(){
-    const [wishList, setwishList] = useState([])
 
-    async function getwishList() {
-        let url = `${process.env.REACT_APP_GAMES_URL}/getAllWishList`;
-        console.log(11111,url)
-        let response = await fetch(url,{
-            method:'Get',
-        });
+    // const commentRef=useRef();
+
+
+   const [wishGame,setWishGame]=useState([])
+
+
+
+    async function getWishList(){
+        
+
+
+        let url=`${process.env.REACT_APP_GAMES_URL}/getAllWishList`
+        let response= await fetch(url,{
+            method:'GET',
+        })
         let recivedData = await response.json();
-        setwishList(recivedData);
-        console.log(11111,wishList)
+        setWishGame(recivedData)
+           
+        
     }
 
-    useEffect(() => {
-        getwishList();
-    }, []);
+    async function handleDelete(id){
+        let url=`${process.env.REACT_APP_GAMES_URL}/DELETE/${id}`
+        let response=await fetch(url,{
+            method:"DELETE", 
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+    
+      
+      if(response.status===204){
+        alert ("successfuly Deleted !!");
+        getWishList();
+       
+      }
+    }
 
+
+    useEffect(()=>{
+        getWishList();
+        // console.log(77,favoriteMovie);
+    },[])
+    
     return(
         <>
-        
-        <section id="wishListySec">
         <h1>Wish List</h1>
-        <div class="divCont">
         {
-            wishList && wishList.map(data=>{
+            wishGame && wishGame.map(game=>{
                 return(
-                    <WishListCard  data={data} />
+                    <Card style={{width:"20rem"}}>
+                    <Card.Img varient='top' src={game.image} alt={game.title}/>
+                    <Card.Body>
+                       <Card.Title>{game.title}</Card.Title>
+                       <Card.Text>{game.description}</Card.Text>
+                       
+                       <Button variant="danger" onClick={()=>handleDelete(game.id)} >Delete</Button><br /><br />
+                       {/* <Button variant="warning" onClick={()=>handleUpdate(movie.id)} >Update comment</Button> */}
+    
+                    </Card.Body>
+              </Card>
                 )
-            
-                
             })
         }
-         </div>
-         <br></br>
-        </section>
         </>
     );
 }
