f971cb8746d985073724276771b2575c733bef35
Sakherr <sakhershteyat@gmail.com>
2023-04-17 20:45:18 +0000

    fixing errors

--- src/components/ModelGame/ModelGame.js
+++ src/components/ModelGame/ModelGame.js
@@ -1,7 +1,6 @@
 
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import { Row, Col } from 'react-bootstrap';
 import './ModelGame.css'
